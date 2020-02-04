5b6668579fa03d0f009db97225a05863e25c1460
Eduardo Meister <eduardo.meister@galileo.edu>
2020-02-04 11:42:25 -0600

    Tarea actividad 2 jsbin

---
+++ mes.js
@@ -0,0 +1,31 @@
+var mes = 10;
+    nombre = "Eduardo";
+    apellido = "Meister";
+    carne = "20006044";
+  if ((mes == 12))
+    document.write("Es Invierno");
+    else if ((mes == 1))
+      document.write("Es Invierno");
+    else if ((mes == 2))
+      document.write("Es Invierno");
+    else if ((mes == 3))
+      document.write("Es Primavera");
+    else if ((mes == 4))
+      document.write("Es Primavera");
+    else if ((mes == 5))
+      document.write("Es Primavera");
+    else if ((mes == 6))
+      document.write("Es Verano");
+    else if ((mes == 7))
+      document.write("Es Verano");
+    else if ((mes == 8))
+      document.write("Es Verano");
+    else if ((mes == 9))
+      document.write("Es Otono");
+    else if ((mes == 10))
+      document.write("Es Otono");
+    else if ((mes == 11))
+      document.write("Es Otono");
+  alert(nombre +" "+ apellido)
+  alert("Carne"+" "+carne)
+    ;
